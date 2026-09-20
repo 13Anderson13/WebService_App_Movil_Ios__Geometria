c2427f43ff72d2d96e4b69392693f3ca5c15de42
13Anderson13 <stevengonzalez6666@gmail.com>
2026-09-20 17:33:08 -0600

    Menu dinamico para la app de iOS

--- Servicios/Minijuegos/ServiceMinijuegos.cs
+++ Servicios/Minijuegos/ServiceMinijuegos.cs
@@ -1,4 +1,5 @@
-﻿using WebServiceGeometria.Respuestas.Minijuegos.Vistas;
+﻿using MySql.Data.MySqlClient;
+using WebServiceGeometria.Respuestas.Minijuegos.Vistas;
 namespace WebServiceGeometria.Servicios.Minijuegos
 {
     public class ServiceMinijuegos
@@ -7,9 +8,36 @@ namespace WebServiceGeometria.Servicios.Minijuegos
         public ServiceMinijuegos(IConfiguration configuration)
         {
             _connectionString = configuration.GetConnectionString("DefaultConnection");
-
-            //public List<vConsultarMinijuegos>
-
         }
+        public List<vConsultarMinijuegos> getMenu()
+        {
+            List<vConsultarMinijuegos> minijuegos = new List<vConsultarMinijuegos>();
+
+            using (MySqlConnection consulta = new MySqlConnection(_connectionString))
+            {
+                consulta.Open();
+
+                string query = @" SELECT * FROM V_Consultar_Minijuego WHERE estado = true";
+
+                MySqlCommand cmd = new (query, consulta);
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read()) 
+                {
+                    minijuegos.Add(new vConsultarMinijuegos
+                    { 
+                        id_minijuego = Convert.ToInt32(reader["id_minijuego"]),
+                        nombre_minijuego = reader["nombre_minijuego"].ToString(),
+                        icono = reader["minijuego"].ToString(),
+                        descripcion = reader["descripcion"].ToString()
+                    });
+                }
+
+            }
+
+            return minijuegos;
+        }
+
     }
 }
